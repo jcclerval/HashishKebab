d73af47df8dc9bbc1c796a6069d598671d491d70
HuguesdeF <hugues.deferrieres@gmail.com>
2018-03-01 20:00:39 +0100

    update

--- main.py
+++ main.py
@@ -3,3 +3,12 @@
 import numpy as np
 
 print("Hugues")
+
+class Ride:
+    def __init__(self,ride):
+        self.start = [int(ride[0]), int(ride[1])]
+        self.end = [int(ride[2]), int(ride[3])]
+        self.step_start = int(ride[4])
+        self.step_end = int(ride[5])
+        self.to_do = True
+        
