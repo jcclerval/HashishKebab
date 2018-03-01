d83195964efc850effdff5d2850e73dbd78a3a51
Jcclerval <jcclerval@aol.com>
2018-03-01 19:42:30 +0100

    test

--- lecture.py
+++ lecture.py
@@ -7,36 +7,3 @@ Created on Thu Mar 01 18:52:11 2018
 
 #with open('README.md','r') as raw_exemple:
 
-def read(entryFile):
-    with open(entryFile,'r') as raw_exemple:
-       data = raw_exemple.read().split('\n')
-       entry = data[0].split(' ')                              # Entrée dans le code
-       print entry
-       R = entry[0]
-       C = entry[1]
-       F = entry[2]
-       N = entry[3]
-       B = entry[4]
-       T = entry[5]
-    
-    return [R, C, F, N, B, T]
-    
-def writeScore(outputData, outputFile):
-    with open(outputFile, 'w') as outFile:
-        for element in outputData:
-            a = str(len(element))
-            for ride in element:
-                a = a + ' ' + str(ride)
-            outFile.write(a+'\n')
-    return 0
-   
-if __name__ == '__main__':
-    
-    entryFile = 'input_data/a_example.in'
-    sortie = read(entryFile)
-    print sortie
-    
-    outputFile = 'jc/out.out'
-    outputData = [[1,2], [3,4], [4,5]]
-    
-    writeScore(outputData, outputFile)
