88ff13acac7545260d01e46dc0c879bcedc4f99b
HuguesdeF <hugues.deferrieres@gmail.com>
2018-03-01 20:11:53 +0100

    update

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 import numpy as np
 
 print("Hugues")
-
 class Ride:
     def __init__(self,ride):
         self.start = [int(ride[0]), int(ride[1])]
@@ -11,4 +10,14 @@ class Ride:
         self.step_start = int(ride[4])
         self.step_end = int(ride[5])
         self.to_do = True
-        
+        self.index = 0
+
+list_obj_arr = []
+
+i = 0
+for elmt in arr:
+    i +=1
+    a = Ride(elmt)
+    a.index = i
+    list_obj_arr.append(a)
+    
