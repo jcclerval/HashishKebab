a485ff16590e3c8a36483547a515538545628e00
Jcclerval <jcclerval@aol.com>
2018-03-01 19:12:28 +0100

    Update

--- lecture.py
+++ lecture.py
@@ -6,15 +6,23 @@ Created on Thu Mar 01 18:52:11 2018
 """
 
 #with open('README.md','r') as raw_exemple:
-with open('input_data/a_example.in','r') as raw_exemple:
-   data = raw_exemple.read().split('\n')
-   entry = data[0].split(' ')                              # Entrée dans le code
+
+def read(entryFile):
+    with open(entryFile,'r') as raw_exemple:
+       data = raw_exemple.read().split('\n')
+       entry = data[0].split(' ')                              # Entrée dans le code
+       print entry
+       R = entry[0]
+       C = entry[1]
+       F = entry[2]
+       N = entry[3]
+       B = entry[4]
+       T = entry[5]
+    
+    return [R, C, F, N, B, T]
    
-   R = entry[0]
-   C = entry[1]
-   F = entry[2]
-   N = entry[3]
-   B = entry[4]
-   T = entry[5]
-   
-   
+if __name__ == '__main__':
+    
+    entryFile = 'input_data/a_example.in'
+    sortie = read(entryFile)
+    print sortie
