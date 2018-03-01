76f5d03cdeffd9a589958d88bc3b21ba12445be5
Jcclerval <jcclerval@aol.com>
2018-03-01 19:17:57 +0100

    Update

--- bilbiHash.py
+++ bilbiHash.py
@@ -7,8 +7,14 @@ Created on Thu Mar 01 19:14:56 2018
 
 def getScore(a,b,x,y):
     return (abs(y-b) + abs(y-x))
+
+def getLatestStart(a,b,x,y,s,f):
+    distance = getScore(a,b,x,y)
+    
+    return f - distance
     
     
 if __name__ == '__main__':
     
     print getScore(0,0,1,2)
+    print getLatestStart(0,0,1,2,1,9)
